f4d300718710f46d949a232aa66e5f4cc9dff83b
korachof <korachof@gmail.com>
2025-07-02 15:40:25 -0700

    made a small change for app.jsx and changed it to App.jsx to follow naming conventions for javascript components.
    Also added boilerplate code to App.jsx, just to have something basic I can eventually run.

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,13 @@
 // Root app component
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to TrashQuest ♻️</h1>
+    </div>
+  );
+}
+
+export default App;
