03a8e1183a98d0a42ed34b61d8625fb5d7136e3a
korachof <korachof@gmail.com>
2025-07-02 16:02:28 -0700

    Installed react and all that, which created a package.json. I then added dev: vite to the scripts package to utilize its development server.

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
 // main app entry point (renders app)
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
