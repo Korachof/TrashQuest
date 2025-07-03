40e9bd46c3da9e083d4029cf2a1c25634ed87755
korachof <korachof@gmail.com>
2025-07-03 11:55:07 -0700

    Added basic code to Header.jsx to create a simple header I can start working from.
    Imported Header.jsx and added basic code to return the header in HomePage.jsx.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 // Landing page/screen for users
-
+import Header from '../components/layout/Header';
 import React from 'react';
 
 function HomePage() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>Welcome to TrashQuest ♻️</h1>
-      <p>Clean the planet, one collectible at a time.</p>
-    </div>
+    <>
+        <Header />
+        <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+            <h1>Welcome to TrashQuest ♻️</h1>
+            <p>Clean the planet, one collectible at a time.</p>
+        </div>
+    </>
   );
 }
 
