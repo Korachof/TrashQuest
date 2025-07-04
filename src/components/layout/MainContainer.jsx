edf23a037b8e4722733e2c193dbe3fbb13f2b705
korachof <korachof@gmail.com>
2025-07-04 15:27:12 -0700

    Since I just added code to my second page, I thought this is a good time to add code to the MainContainer file to stick to DRY principles. Don't want to get too ahead of myself and continue to repeat myself.

--- src/components/layout/MainContainer.jsx
+++ src/components/layout/MainContainer.jsx
@@ -1,1 +1,18 @@
 // Wrapper to apply consistent layouts, padding, and backgrounds across views and pages
+
+function MainContainer({ children }) {
+  return (
+    <main style={{
+      textAlign: 'center',
+      marginTop: '4rem',
+      padding: '0 1rem',
+      maxWidth: '700px',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }}>
+      {children}
+    </main>
+  );
+}
+
+export default MainContainer;
