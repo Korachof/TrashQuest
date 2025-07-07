b19b285a4ab0256c0135c8d45993fffdda3a9ce4
korachof <korachof@gmail.com>
2025-07-07 09:59:29 -0700

    Did a small bit of visual cleanup.
    Added a link to navigate back to the login page in case the user already has an account and accidentally found themselves on this page.

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageLayout from '../components/layout/PageLayout';
+
 
 function SignupPage() {
   return (
     <PageLayout>
-      <h1>Sign Up for TrashQuest 🌱</h1>
-      <p>Create your eco profile and start cleaning the planet one quest at a time.</p>
-      {/* Future: Add signup form fields here */}
+        <h1>Sign Up for TrashQuest 🌱</h1>
+        <p>Create your eco profile and start cleaning the planet one quest at a time.</p>
+        {/* Future: Add signup form fields here */}
+        <p style={{ marginTop: '2rem' }}>
+        Already have an account? <Link to="/login">Log in here</Link>
+        </p>
     </PageLayout>
   );
 }
