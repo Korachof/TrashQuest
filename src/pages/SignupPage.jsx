0c3adc28493b16b3e311d8300773c6e8853062d6
korachof <korachof@gmail.com>
2025-07-07 09:40:24 -0700

    Created a basic boilerplate SignupPage.jsx in src/pages. This will link from the LoginPage and give new users a chance to sign up. Next, I'll create routes and such that connect this to the rest of the app and create flow.

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageLayout from '../components/layout/PageLayout';
+
+function SignupPage() {
+  return (
+    <PageLayout>
+      <h1>Sign Up for TrashQuest 🌱</h1>
+      <p>Create your eco profile and start cleaning the planet one quest at a time.</p>
+      {/* Future: Add signup form fields here */}
+    </PageLayout>
+  );
+}
+
+export default SignupPage;
