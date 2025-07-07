74d5f3869d9c9530599f1733561f7bd1e9928121
korachof <korachof@gmail.com>
2025-07-07 12:40:10 -0700

    Went through a bunch of crap and debugging to get the display fields to center correctly. The input boxes kept aligning left, and most everything else in the block was center aligning with the fields instead of the page, so it was displaying very strangely.
    Fixed all of that.

--- src/components/shared/FormGroup.jsx
+++ src/components/shared/FormGroup.jsx
@@ -4,16 +4,22 @@ import React from 'react';
 
 function FormGroup({ label, type, value, onChange, required = true }) {
   return (
-    <label style={{ marginTop: '1rem', width: '100%' }}>
-      {label}
-      <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        required={required}
-        style={{ display: 'block', width: '100%', marginTop: '0.5rem' }}
-      />
-    </label>
+    <div style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <label style={{ marginTop: '1rem', width: '100%', textAlign: 'center', }}>
+            {label}
+        </label>
+        <input
+            type={type}
+            value={value}
+            onChange={onChange}
+            required={required}
+            style={{
+            width: '100%',
+            padding: '0.5rem',
+            marginTop: '0.5rem',
+            boxSizing: 'border-box'}}
+        />
+  </div>
   );
 }
 
