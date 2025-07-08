b2483a63aba6c301f4a18cd0cce36c463e2b0079
korachof <korachof@gmail.com>
2025-07-08 09:48:40 -0700

    Added display fields and labels to LoginPage to match SignupPage. Used FormGroup and the same form styling to keep a consistency between the pages.
    Next step is to refactor to avoid violating DRY, since I'm using the same form styling for both.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,46 @@
 // Page for user to login to their profile.
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageLayout from '../components/layout/PageLayout';
+import FormGroup from '../components/shared/FormGroup';
 
 
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup submitted:', { displayName, email, password });
+    // TODO: Hook into Firebase/AWS logic
+  };
   return (
     // PageLayout: Page scaffold including header, MainContainer, and footer
     <PageLayout>
       <h1>Login to Your Quest 🔐</h1>
       <p>Access your dashboard, earn ecoPoints, and track your progress.</p>
+      <form onSubmit={handleSubmit}
+          style={{
+            margin: '2rem auto',
+            width: '100%',
+            maxWidth: '400px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '0.5rem'}}>
+        <FormGroup
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <FormGroup
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
       <p style={{ marginTop: '2rem' }}>
       Don't have an account? <Link to="/signup">Sign up here</Link>
       </p>
