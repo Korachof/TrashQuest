a21c07c3b8498f919ee05e77e40f8c00d60759c3
korachof <korachof@gmail.com>
2025-07-10 11:24:20 -0700

    Noticed that the Footer wasn't anchored to the bottom of the viewport, and would instead float if there wasn't much content.
    Did two things. I added a div and some styling to PageLayout to clean it up a bit and ensure vertical stacking.
    
    Then I added a flexGrow: 1 to MainContainer, so all pages will share this behavior without much issue.
    
    If I end up needing to re-use PageLayout and this is a problem, I can add it to PageLayout with props or something, but this is so much cleaner. I really doubt I'll re-use PageLayout for future cards and such anyway, but it's something to keep in mind for the future.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -5,12 +5,18 @@ import MainContainer from './MainContainer';
 
 function PageLayout({ children }) {
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
       <Header />
       {/*Page style wrapper*/}
       <MainContainer>{children}</MainContainer>
       <Footer />
-    </>
+    </div>
   );
 }
 
