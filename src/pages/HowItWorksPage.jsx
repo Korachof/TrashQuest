efce97f02e79394a61f070f28e7161c0bfab4d04
korachof <korachof@gmail.com>
2025-07-10 09:44:05 -0700

    Created a How it Works page and placed it in the Header.
    Added the corresponding routes to App.jsx
    
    Added the page links to the Header.jsx.
    
    Tested everything, and it works so far. All pages are connected correctly.
    
    I'm not sure how I feel about the right-justification of the elements, however. I'll fill it out first and then make design decisions, but I'm assuming I'll need to wrap it all up into a div style or something so the logo and nav play nice with each other instead of against each other as separate elements.

---
+++ src/pages/HowItWorksPage.jsx
@@ -0,0 +1,16 @@
+// How it Works page
+import React from 'react';
+import PageLayout from '../components/layout/PageLayout';
+
+
+function HowItWorksPage() {
+  return (
+    <PageLayout>
+      <h1>How It Works</h1>
+      <p>Learn how Trash Quest Works here!</p>
+    </PageLayout>
+  );
+}
+
+
+export default HowItWorksPage;
