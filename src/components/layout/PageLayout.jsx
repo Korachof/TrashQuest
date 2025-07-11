6e7b9e4568c265d2f98d3fd5ceaff924a7cf2645
korachof <korachof@gmail.com>
2025-07-11 07:53:49 -0700

    Did some light refactoring with PageLayout and MainContainer. Removed the div/main styling and added it to styles/layout.js. Then I imported the const styles and used them as wrappers in both files.
    Also changed a few loose threads from yesterday's scrollbar search. Simplified some of the elements like paddingLeft, etc. to just padding. Same with margin.
    
    Tested and it works fine and as expected.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -2,16 +2,11 @@ import Header from './Header';
 import Footer from './Footer';
 import React from 'react';
 import MainContainer from './MainContainer';
+import { layoutPageStyleWrapper } from '../../styles/layout';
 
 function PageLayout({ children }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100svh',
-      }}
-    >
+    <div style={layoutPageStyleWrapper}>
       <Header />
       {/*Page style wrapper*/}
       <MainContainer>{children}</MainContainer>
