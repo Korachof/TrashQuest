483ac213e0404e6c99d1a79afa65fae9d78c79fa
korachof <korachof@gmail.com>
2025-07-11 07:53:49 -0700

    Did some light refactoring with PageLayout and MainContainer. Removed the div/main styling and added it to styles/layout.js. Then I imported the const styles and used them as wrappers in both files.
    Also changed a few loose threads from yesterday's scrollbar search. Simplified some of the elements like paddingLeft, etc. to just padding. Same with margin.
    
    Tested and it works fine and as expected.

--- src/components/layout/MainContainer.jsx
+++ src/components/layout/MainContainer.jsx
@@ -1,23 +1,11 @@
 // Wrapper to apply consistent layouts, padding, and backgrounds across views and pages
 import React from 'react';
+import { mainContainerStyleWrapper } from '../../styles/layout';
 
 function MainContainer({ children }) {
   return (
     // Normal page layout styling
-    <main
-      style={{
-        textAlign: 'center',
-        paddingTop: '4rem',
-        paddingLeft: '1rem',
-        paddingRight: '1rem',
-        maxWidth: '700px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        flexGrow: 1,
-      }}
-    >
-      {children}
-    </main>
+    <main style={mainContainerStyleWrapper}>{children}</main>
   );
 }
 
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,5 +1,24 @@
 // Handles layout/structural styling patterns throughout the app
 
+const layoutPageStyleWrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  boxSizing: 'border-box',
+  margin: 0,
+  padding: 0,
+  overflow: 'clip',
+};
+
+const mainContainerStyleWrapper = {
+  textAlign: 'center',
+  flexGrow: 1,
+  maxWidth: '700px',
+  margin: '0 auto',
+  padding: '4rem 1rem 0',
+  boxSizing: 'border-box',
+};
+
 const navContainer = {
   display: 'flex',
   justifyContent: 'center',
@@ -9,4 +28,4 @@ const navContainer = {
   marginBottom: '0.5rem',
 };
 
-export { navContainer };
+export { layoutPageStyleWrapper, mainContainerStyleWrapper, navContainer };
