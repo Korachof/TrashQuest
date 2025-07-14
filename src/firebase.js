aa3b8b814b60c184adf41e279e92751bf8508f44
korachof <korachof@gmail.com>
2025-07-14 09:28:00 -0700

    Installed firebase into the project, and created a firebase.js with the provided boilerplate snippet. So far, the project still works the same, so nothing appears to be broken or incorrect at this stage.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Trash Quest's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgyF82k0fyI5xg_4yCN6xtahYXeomUKzo',
+  authDomain: 'trashquest-mvp.firebaseapp.com',
+  projectId: 'trashquest-mvp',
+  storageBucket: 'trashquest-mvp.firebasestorage.app',
+  messagingSenderId: '109397133155',
+  appId: '1:109397133155:web:d1a7ddece38dbda35ef556',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
