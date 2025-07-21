ea990d4602e031ae3257311bab7092de207072b0
korachof <korachof@gmail.com>
2025-07-21 08:59:28 -0700

    Updated PageLayout with the AuthContext so it will grab the last updated user. Cleaned up and refactored, and after some light testing it seems to work as intended.

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -3,11 +3,13 @@ import Footer from './Footer';
 import React from 'react';
 import MainContainer from './MainContainer';
 import { layoutPageStyleWrapper } from '../../styles/layout';
+import { useAuth } from '../../context/AuthContext';
 
-function PageLayout({ children, currentUser }) {
+function PageLayout({ children }) {
+  const { currentUser } = useAuth();
   return (
     <div style={layoutPageStyleWrapper}>
-      <Header currentUser={currentUser} />
+      <Header />
       {/*Page style wrapper*/}
       <MainContainer>{children}</MainContainer>
       <Footer />
