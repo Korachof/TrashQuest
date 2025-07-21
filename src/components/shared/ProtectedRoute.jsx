c91a09becabb688ba441de1fc183f0efbdd2b96a
korachof <korachof@gmail.com>
2025-07-21 09:08:16 -0700

    Updated ProtectedRoute with Authcontext to grab the last updated user. Also imported the Loader message in casse the user hasn't been quite updated yet and its supposed to navigate and doesn't yet.

--- src/components/shared/ProtectedRoute.jsx
+++ src/components/shared/ProtectedRoute.jsx
@@ -1,8 +1,17 @@
 // Protects certain routes from being accessed if user isn't logged in/no authentication
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import Loader from './Loader';
 
-function ProtectedRoute({ currentUser, children }) {
+function ProtectedRoute({ children }) {
+  // Grab last updated user
+  const { currentUser, authLoading } = useAuth();
+
+  // if user hasn't finished updating, display loading message
+  if (authLoading) {
+    return <Loader />;
+  }
   if (!currentUser) {
     return <Navigate to="/login" replace />;
   }
