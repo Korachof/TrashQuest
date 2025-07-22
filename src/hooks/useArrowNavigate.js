3a3cce87a9ecdc9a9ffeeedcc746a6b947c71a33
korachof <korachof@gmail.com>
2025-07-22 08:02:09 -0700

    Created the simple useArrowNavigation scaffold file, so we have it when it's ready to be coded and implemented.

---
+++ src/hooks/useArrowNavigate.js
@@ -0,0 +1,7 @@
+// Allows user to use arrow keys to navigate various processes and modals
+import React from 'react';
+
+export default function useArrowNavigate(refs) {
+  // TODO: Handle ArrowLeft / ArrowRight to cycle between refs
+  // TODO: Handle Enter to trigger .click() on focused element
+}
