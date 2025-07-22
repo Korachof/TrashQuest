1243f3f3e7c3fbf67b8a3c50792003bdd0806507
korachof <korachof@gmail.com>
2025-07-22 07:51:27 -0700

    Added useEscape call to ConfirmLogout, which passes in onCancel to cancel the logout confirmation modal when the escape key is used. This will make it easier for accessibility purposes.

--- src/components/shared/ConfirmLogout.jsx
+++ src/components/shared/ConfirmLogout.jsx
@@ -1,9 +1,11 @@
 // Pop up that confirms the user's logout request
 import React from 'react';
+import useEscape from '../../hooks/useEscape';
 
 import { popupStyles } from '../../styles/layout';
 
 function ConfirmLogout({ onConfirm, onCancel }) {
+  useEscape(onCancel);
   return (
     <div style={popupStyles}>
       <p>Are you sure you want to log out?</p>
