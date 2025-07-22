c48a7c98ae30fb868616ae352a1619506c59f837
korachof <korachof@gmail.com>
2025-07-22 09:37:04 -0700

    Added in scaffolded code for imports and a describe block with a few needed tests for Protected Route. Still need to code in the logic, but the organization is there.

--- src/components/shared/__tests__/ProtectedRoute.test.jsx
+++ src/components/shared/__tests__/ProtectedRoute.test.jsx
@@ -1,1 +1,20 @@
 // Tester for the /shared/ProtectedRoute.jsx component
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProtectedRoute from '../ProtectedRoute';
+import { AuthContext } from '../../contexts/AuthContext';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('ProtectedRoute', () => {
+  test('renders children if authenticated', () => {
+    // logic
+  });
+
+  test('redirects to default route if unauthenticated', () => {
+    // logic
+  });
+
+  test('redirects to custom route if redirectTo prop is provided', () => {
+    // logic
+  });
+});
