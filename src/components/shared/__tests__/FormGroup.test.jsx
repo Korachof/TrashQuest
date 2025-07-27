e82f8c830780e56564e7a35c7a35ef21acbbef2b
korachof <korachof@gmail.com>
2025-07-27 13:16:00 -0700

    Added 3 test functions to FormGroup component tests. There's a lot more to test, but just going a few at a time, and testing as I go.
    Test 1: Verifies if the form label text renders
    
    test 2: Verifies that type email in FormGroup <input> renders correctly
    
    test 3: Verifies that type password in FormGroup <input> renders correctly.
    
    I was going to just have one type test, but I realized that the type could be hardcoded "email," so I wanted to test at least two types. I'll keep these boundaries in mind as I create more, similar tests for FormGroup.

--- src/components/shared/__tests__/FormGroup.test.jsx
+++ src/components/shared/__tests__/FormGroup.test.jsx
@@ -1,5 +1,6 @@
 // Tests for the FormGroup component
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import FormGroup from '../FormGroup';
 import { vi } from 'vitest';
 
@@ -11,4 +12,31 @@ describe('FormGroup', () => {
     onChange: vi.fn(), // reusable mock
     id: 'email-input',
   };
+
+  // clear the reused mock onChange() before each test
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  // Test 1: Verifies if the form label text renders correctly
+  test('renders label text correctly', () => {
+    render(<FormGroup {...defaultProps} />);
+    expect(screen.getByText('Email')).toBeInTheDocument();
+  });
+
+  // Test 2: Verifies that type email in FormGroup <input> renders correctly
+  test('renders email input correctly', () => {
+    render(<FormGroup {...defaultProps} type="email" />);
+    expect(screen.getByRole('textbox')).toHaveAttribute('type', 'email');
+  });
+
+  /* Test 3: Verifies that type password in FormGroup <input> renders correctly
+     Second 'type' test is necessary to confirm type isn't hardcoded */
+  test('renders password input correctly', () => {
+    render(<FormGroup {...defaultProps} type="password" />);
+    expect(screen.getByLabelText(defaultProps.label)).toHaveAttribute(
+      'type',
+      'password'
+    );
+  });
 });
