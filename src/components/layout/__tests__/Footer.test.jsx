3178eb184c4d685ab7e76319bd54143e3827bdd3
korachof <korachof@gmail.com>
2025-07-31 12:44:53 -0700

    Added more tests to footer to verify that the href nav link attribute is correct. Restructred the tests so link tests are organized by the Text of the link (so all Terms of Use tests are grouped  together, for example). This will make debugging in the future easier, and if any changes need to be made to a link, name, etc., it should be simple to find and change in all the correct places without having to navigate the file.

--- src/components/layout/__tests__/Footer.test.jsx
+++ src/components/layout/__tests__/Footer.test.jsx
@@ -26,26 +26,66 @@ describe('Footer', () => {
   });
 
   // Test 2: Verifies that the Terms of Use nav link is rendering
-  test('renders Terms of Use link', () => {
+  test('terms of use link renders', () => {
     render(<Footer />);
     expect(screen.getByText('Terms of Use')).toBeInTheDocument();
   });
 
-  // Test 3: Verifies that the Privacy Policy nav link is rendering
-  test('renders Privacy Policy link', () => {
+  // Test 3: Verifies that Terms of Use Nav link has correct href attribute
+  test('terms of use nav link has correct href attribute', () => {
+    render(<Footer />);
+
+    expect(screen.getByText('Terms of Use').closest('a')).toHaveAttribute(
+      'href',
+      '/terms'
+    );
+  });
+
+  // Test 4: Verifies that the Privacy Policy nav link is rendering
+  test('privacy policy link renders', () => {
     render(<Footer />);
     expect(screen.getByText('Privacy Policy')).toBeInTheDocument();
   });
 
-  // Test 4: Verifies that the About Us nav link is rendering
-  test('renders About Us link', () => {
+  // Test 5: Verifies that Privacy Policy Nav link has correct href attribute
+  test('privacy policy nav link has correct href attribute', () => {
+    render(<Footer />);
+
+    expect(screen.getByText('Privacy Policy').closest('a')).toHaveAttribute(
+      'href',
+      '/privacy'
+    );
+  });
+
+  // Test 6: Verifies that the About Us nav link is rendering
+  test('about us link renders', () => {
     render(<Footer />);
     expect(screen.getByText('About Us')).toBeInTheDocument();
   });
 
-  // Test 5: Verifies that the Contact Us nav link is rendering
-  test('renders Contact Us link', () => {
+  // Test 7: Verifies that About Us Nav link has correct href attribute
+  test('about us nav link has correct href attribute', () => {
+    render(<Footer />);
+
+    expect(screen.getByText('About Us').closest('a')).toHaveAttribute(
+      'href',
+      '/about'
+    );
+  });
+
+  // Test 8: Verifies that the Contact Us nav link is rendering
+  test('contact us link renders', () => {
     render(<Footer />);
     expect(screen.getByText('Contact Us')).toBeInTheDocument();
+  });
+
+  // Test 9: Verifies that Privacy Policy Nav link has correct href attribute
+  test('contact us nav link has correct href attribute', () => {
+    render(<Footer />);
+
+    expect(screen.getByText('Contact Us').closest('a')).toHaveAttribute(
+      'href',
+      '/contact'
+    );
   });
 });
