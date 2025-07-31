de168315ea097b895508951cba9f6746884c4aee
korachof <korachof@gmail.com>
2025-07-31 13:18:22 -0700

    Added the last remaining tests for Footer, and reorganized tests based on function/link.
    Basically, I added a test to verify that the link elements are inside of the nav elements, and not moved or placed elsewhere. This test isn't the MOST helpful, but it verifies that these links aren't for some reason moved outside of nav on accident.
    
    I created one of these tests for all 4 elements, and grouped them by link text name.
    
    All 18 tests are passing!

--- src/components/layout/__tests__/Footer.test.jsx
+++ src/components/layout/__tests__/Footer.test.jsx
@@ -49,13 +49,22 @@ describe('Footer', () => {
     );
   });
 
-  // Test 5: Verifies that the Privacy Policy nav link is rendering
+  // Test 5: Verifies that the nav element contains the terms of use link element
+  test('terms of use nav link is inside nav element', () => {
+    render(<Footer />);
+    const navElement = screen.getByRole('navigation');
+    const termsLink = screen.getByText('Terms of Use');
+
+    expect(navElement).toContainElement(termsLink);
+  });
+
+  // Test 6: Verifies that the Privacy Policy nav link is rendering
   test('privacy policy link renders', () => {
     render(<Footer />);
     expect(screen.getByText('Privacy Policy')).toBeInTheDocument();
   });
 
-  // Test 6: Verifies that Privacy Policy Nav link has correct href attribute
+  // Test 7: Verifies that Privacy Policy Nav link has correct href attribute
   test('privacy policy nav link has correct href attribute', () => {
     render(<Footer />);
 
@@ -65,13 +74,22 @@ describe('Footer', () => {
     );
   });
 
-  // Test 7: Verifies that the About Us nav link is rendering
+  // Test 8: Verifies that the nav element contains the Privacy Policy link element
+  test('privacy policy nav link is inside nav element', () => {
+    render(<Footer />);
+    const navElement = screen.getByRole('navigation');
+    const policyLink = screen.getByText('Privacy Policy');
+
+    expect(navElement).toContainElement(policyLink);
+  });
+
+  // Test 9: Verifies that the About Us nav link is rendering
   test('about us link renders', () => {
     render(<Footer />);
     expect(screen.getByText('About Us')).toBeInTheDocument();
   });
 
-  // Test 8: Verifies that About Us Nav link has correct href attribute
+  // Test 10: Verifies that About Us Nav link has correct href attribute
   test('about us nav link has correct href attribute', () => {
     render(<Footer />);
 
@@ -81,13 +99,22 @@ describe('Footer', () => {
     );
   });
 
-  // Test 9: Verifies that the Contact Us nav link is rendering
+  // Test 11: Verifies that the nav element contains the About Us link element
+  test('about us nav link is inside nav element', () => {
+    render(<Footer />);
+    const navElement = screen.getByRole('navigation');
+    const aboutLink = screen.getByText('About Us');
+
+    expect(navElement).toContainElement(aboutLink);
+  });
+
+  // Test 12: Verifies that the Contact Us nav link is rendering
   test('contact us link renders', () => {
     render(<Footer />);
     expect(screen.getByText('Contact Us')).toBeInTheDocument();
   });
 
-  // Test 10: Verifies that Privacy Policy Nav link has correct href attribute
+  // Test 13: Verifies that Contact Us nav link has correct href attribute
   test('contact us nav link has correct href attribute', () => {
     render(<Footer />);
 
@@ -97,7 +124,16 @@ describe('Footer', () => {
     );
   });
 
-  // Test 11: Verifies that the Footer tagline text renders
+  // Test 14: Verifies that the nav element contains the Contact Us link element
+  test('privacy policy nav link is inside nav element', () => {
+    render(<Footer />);
+    const navElement = screen.getByRole('navigation');
+    const contactLink = screen.getByText('Contact Us');
+
+    expect(navElement).toContainElement(contactLink);
+  });
+
+  // Test 15: Verifies that the Footer tagline text renders
   test('trashquest tagline text renders', () => {
     render(<Footer />);
 
@@ -106,21 +142,21 @@ describe('Footer', () => {
     ).toBeInTheDocument();
   });
 
-  // Test 12: Verifies that the copyright text renders
+  // Test 16: Verifies that the copyright text renders
   test('copyright text renders', () => {
     render(<Footer />);
 
     expect(screen.getByText(/TrashQuest/)).toBeInTheDocument();
   });
 
-  // Test 13: Verifies that the first copyright year (2025) renders
+  // Test 17: Verifies that the first copyright year (2025) renders
   test('displays first copyright year (2025) in copyright', () => {
     render(<Footer />);
 
     expect(screen.getByText(/2025/)).toBeInTheDocument();
   });
 
-  // Test 14: Verifies that the current year is displayed in copyright
+  // Test 18: Verifies that the current year is displayed in copyright
   test('displays current year in copyright', () => {
     const currentYear = new Date().getFullYear();
     render(<Footer />);
