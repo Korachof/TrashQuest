18a6f7b49550f375d871303bab0b5b0471299d5c
korachof <korachof@gmail.com>
2025-08-07 08:47:40 -0700

    Created a PublicOnlyRoute.jsx file that should work the same as my current RedirectIfAuthenticated file. I'm going to slowly attach it to the routes and remove RedirectIf one by one after this.

---
+++ src/components/shared/PublicOnlyRoute.jsx
@@ -0,0 +1,25 @@
+// redirects logged in users if they try to access certain public only areas, like WelcomePage
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
+import Loader from './Loader';
+
+export default function RedirectIfAuthenticated({
+  children,
+  redirectTo = '/dashboard',
+}) {
+  const { currentUser, authLoading } = useAuth();
+
+  // If still loading, show loader
+  if (authLoading) {
+    return <Loader />;
+  }
+
+  // If user is authenticated, redirect them away
+  if (currentUser) {
+    return <Navigate to={redirectTo} replace />;
+  }
+
+  // If not authenticated, render the page
+  return children;
+}
