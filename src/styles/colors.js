fe5ad3dc67169f6e1a1fb1db532b3f1275e65e2d
korachof <korachof@gmail.com>
2025-08-19 08:25:55 -0700

    Created a colors styles folder that will be reused throughout my compoonents. Will have colors for primary and secondary app colors, button colors, text colors, etc.

---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+// Colors file for reusable colors throughout the app
+
+export const color = {
+  // App Primary Colors
+  primaryButton: '#28a745',
+
+  // App Secondary Colors
+  secondaryButton: '#6c757d',
+
+  // Text Colors
+  headingTextColor: '#333',
+  bodyTextColor: '#444',
+  buttonTextColor: '#ffffff',
+};
