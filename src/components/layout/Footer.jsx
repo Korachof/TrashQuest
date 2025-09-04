f09fcecededd4f4dc5bd99f9dd249caab13f1cd5
korachof <korachof@gmail.com>
2025-09-04 08:22:04 -0700

    Created footerContent.js to abstract the Footer text content and reference it in Footer.jsx. Everything is working as intended.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,21 +3,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { navContainer } from '../../styles/layout';
 import { footerStyleContainer } from '../../styles/layout';
+import { footerContent as content } from '../../content/footerContent';
 
 export default function Footer() {
   return (
     <footer style={footerStyleContainer}>
       {/*Nav Links*/}
       <nav style={navContainer}>
-        <Link to="/terms">Terms of Use</Link>
-        <Link to="/privacy">Privacy Policy</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact Us</Link>
+        <Link to="/terms">{content.terms}</Link>
+        <Link to="/privacy">{content.policy}</Link>
+        <Link to="/about">{content.about}</Link>
+        <Link to="/contact">{content.contact}</Link>
       </nav>
-      <p>
-        Made for the Ultimate Quest: The Planet🌎 | © {new Date().getFullYear()}{' '}
-        TrashQuest
-      </p>
+      <p>{content.tagLine}</p>
     </footer>
   );
 }
---
+++ src/content/footerContent.js
@@ -0,0 +1,9 @@
+// Footer Content
+
+export const footerContent = {
+  terms: `Terms of Use`,
+  policy: `Privacy Policy`,
+  about: `About Us`,
+  contact: `Contact Us`,
+  tagLine: `Made for the Ultimate Quest: The Planet🌎 | © ${new Date().getFullYear()} TrashQuest`,
+};
