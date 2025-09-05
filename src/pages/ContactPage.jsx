7550e828620471f710a0444a9d9e49e5dd243bcd
korachof <korachof@gmail.com>
2025-09-05 11:59:44 -0700

    Added some basic info to ContactPage.jsx as I continue to work on the ContactForm Component on the side. Haven't pushed that or implemented that yet until it's working. Will also abstract the text content in this in a bit as well.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,12 @@
 // Contact Page -> Lives on Footer
 import React, { useEffect } from 'react';
-import { headingTextStyle, subHeadingTextStyle } from '../styles/typography';
+import {
+  headingTextStyle,
+  subHeadingTextStyle,
+  centerBodyTextStyle,
+  bodyTextStyle,
+} from '../styles/typography';
 import { contactContent } from '../content/contact';
-
 export default function ContactPage() {
   // Set page tab title
   useEffect(() => {
@@ -14,6 +18,15 @@ export default function ContactPage() {
         {contactContent.title}
       </h1>
       <h3 style={subHeadingTextStyle}>{contactContent.subTitle}</h3>
+
+      {/* TODO: create ContactForm and remove this comment except: <ContactForm /> */}
+
+      <div style={centerBodyTextStyle}>
+        <h3 style={subHeadingTextStyle}>Other Ways to Reach Us</h3>
+        <p>You can also reach us through:</p>
+        <p>Email: support@ecoquest.com</p>
+        <p>Follow us on social media: </p>
+      </div>
     </>
   );
 }
