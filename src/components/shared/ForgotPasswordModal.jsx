350fc7fa6f4f2cd22382d222f25841049a4c189b
korachof <korachof@gmail.com>
2025-09-08 07:27:00 -0700

    Changed forgot password modal so that it sends the lower case version of the email back to firebase.

--- src/components/shared/ForgotPasswordModal.jsx
+++ src/components/shared/ForgotPasswordModal.jsx
@@ -24,7 +24,7 @@ export default function ForgotPasswordModal({ isOpen, onClose }) {
     setSuccessMsg('');
 
     try {
-      await sendPasswordResetEmail(auth, email);
+      await sendPasswordResetEmail(auth, email.toLowerCase());
       setSuccessMsg(forgotPasswordContent.successMsg);
       setEmail(''); // Clear the form
     } catch (error) {
